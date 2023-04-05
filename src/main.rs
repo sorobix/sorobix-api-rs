0bfda5c993ad0ab724b7902bb0134f262851ca6f
Hemanth Krishna <hkpdev008@gmail.com>
2023-04-06 00:36:56 +0530

    feat: TraceLayer logging
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use axum::{
 };
 use std::net::SocketAddr;
 use std::sync::Arc;
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::cors::CorsLayer;
+use tower_http::trace::TraceLayer;
 
 use crate::{
     handlers::account_handler::generate_new_account,
@@ -22,6 +23,12 @@ use crate::{
 
 #[tokio::main]
 async fn main() {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var(
+            "RUST_LOG",
+            "sorobix-api-rs=debug,tower_http=debug,server=debug",
+        )
+    }
     println!("Sorobix API RS Booted");
     tracing_subscriber::fmt::init();
     let application_service = ApplicationService::new();
@@ -29,17 +36,14 @@ async fn main() {
         application_service,
     };
     let state = Arc::new(application_state);
-    let cors = CorsLayer::new()
-        .allow_origin(Any)
-        .allow_headers(Any)
-        .allow_methods(Any);
     let app = Router::new()
-        .layer(cors)
         .route("/", get(root))
         .route("/account", post(generate_new_account))
         .route("/compile", post(compile_contract))
         .route("/deploy", post(deploy_contract))
         .route("/invoke", post(invoke_contract))
+        .layer(CorsLayer::permissive())
+        .layer(TraceLayer::new_for_http())
         .with_state(state);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
