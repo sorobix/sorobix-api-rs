954125a00dae932938890e3735ee15071ab8a2e0
Hemanth Krishna <hkpdev008@gmail.com>
2023-04-06 01:59:22 +0530

    chore: wrap router with /api prefix
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ async fn main() {
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(Router::new().nest("/api", app).into_make_service())
         .await
         .unwrap();
 }
